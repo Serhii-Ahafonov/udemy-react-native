01d795f631c4fbcb5b27431ed50aadbde8ff9bac
sahafonov <sahafonov@atlassian.com>
2023-02-28 11:56:26 +0200

    [Meal APP] added MealsCategory and MealItem functionality

---
+++ MealApp/components/CategoryGridTile.js
@@ -0,0 +1,50 @@
+import {Platform, Pressable, StyleSheet, Text, View} from 'react-native';
+
+function CategoryGridTile({title, color, onPress}) {
+  return (
+    <View style={styles.gridItem}>
+      <Pressable
+        style={({pressed}) => [styles.button, pressed ? styles.buttonPressed : null]}
+        onPress={onPress}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.text}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    shadowRadius: 8,
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowColor: 'black',
+    backgroundColor: 'white',
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible'
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 18
+  }
+});
---
+++ MealApp/screens/MealsOverviewScreen.js
@@ -0,0 +1,45 @@
+import { FlatList, StyleSheet, View} from 'react-native';
+import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
+
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  })
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      complexity: item.complexity,
+      affordability: item.affordability,
+      duration: item.duration
+    };
+
+    return (
+      <MealItem {...mealItemProps} />
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        renderItem={renderMealItem}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+}
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16
+  }
+});
