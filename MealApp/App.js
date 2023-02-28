36830a8732e36396ea9c08a5ae8f26cbe6060102
sahafonov <sahafonov@atlassian.com>
2023-02-28 17:08:08 +0200

    [Meal APP] finished navigation

--- MealApp/App.js
+++ MealApp/App.js
@@ -1,4 +1,3 @@
-import { StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer  } from '@react-navigation/native';
@@ -74,7 +73,3 @@ export default function App() {
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {},
-});
